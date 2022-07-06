c0ceaac0deecbf82bf80cf826c65a586f3e46fb1
Anton Tarasov <respawn91@gmail.com>
2022-07-06 20:50:12 +0300

    Add files via upload

---
+++ Lesson-1.java
@@ -0,0 +1,81 @@
+//
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 10;
+      
+      System.out.println(a);
+  }
+}
+
+//
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 10;
+      System.out.println(a);
+      
+      double b = 10;
+      System.out.println(b);
+      
+      String s = "Welcome from UI! Test123@346@!#% @#%$@#";
+      System.out.println(s);
+      
+      boolean d = true;
+      System.out.println(d);
+  }
+}
+
+//
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 10;
+      int b = 20;
+      
+      int c = a + b;
+      System.out.println(c);
+      
+      String s1 = "10";
+      String s2 = "20";
+      
+      String s3 = s1 + s2;
+      System.out.println(s3);
+  }
+}
+
+//
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+      
+      int a = 35;
+      int b = 10;
+      
+      int result1 = a + b;
+      int result2 = a - b;
+      int result3 = a * b;
+      int result4 = a / b;
+      int result5 = a % b;
+      
+      System.out.println(result1);
+      System.out.println(result2);
+      System.out.println(result3);
+      System.out.println(result4);
+      System.out.println(result5);
+  }
+}
+
+//
+
